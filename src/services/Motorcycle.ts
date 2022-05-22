6938cd17668e43ebba5d3bb8484e7b3abf864136
Gustavo 'Biza' Dolzan <gudolzan@gmail.com>
2022-05-22 14:24:03 -0300

    feat: create Motorcycle service

---
+++ src/services/Motorcycle.ts
@@ -0,0 +1,31 @@
+import Service, { ServiceError } from '.';
+import {
+  Motorcycle,
+  MotorcycleSchema,
+} from '../interfaces/MotorcycleInterface';
+import MotorcycleModel from '../models/Motorcycle';
+
+class MotorcycleService extends Service<Motorcycle> {
+  constructor(model = new MotorcycleModel()) {
+    super(model);
+  }
+
+  create = async (
+    obj: Motorcycle,
+  ): Promise<Motorcycle | null | ServiceError> => {
+    const parsed = MotorcycleSchema.safeParse(obj);
+    if (!parsed.success) return { error: parsed.error };
+    return this.model.create(obj);
+  };
+
+  update = async (
+    id: string,
+    obj: Motorcycle,
+  ): Promise<Motorcycle | null | ServiceError> => {
+    const parsed = MotorcycleSchema.safeParse(obj);
+    if (!parsed.success) return { error: parsed.error };
+    return this.model.update(id, obj);
+  };
+}
+
+export default MotorcycleService;
