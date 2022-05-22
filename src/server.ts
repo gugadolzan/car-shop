88b57ae25bbb9fde5ec8d2a5adc570e482b5d215
Gustavo 'Biza' Dolzan <gudolzan@gmail.com>
2022-05-22 14:05:40 -0300

    feat: implement server

---
+++ src/server.ts
@@ -0,0 +1,15 @@
+import App from './app';
+import CarController from './controllers/Car';
+import { Car } from './interfaces/CarInterface';
+import CustomRouter from './routes/Router';
+
+const server = new App();
+
+const carController = new CarController();
+
+const carRouter = new CustomRouter<Car>();
+carRouter.addRoute(carController);
+
+server.addRouter(carRouter.router);
+
+export default server;
